fda61a32a942fb1728db3ea10b616a34b86456ea
Leonardo Rodrigues <leonardo@grupoblackbox.com>
2026-02-17 20:35:56 -0300

    chore: add ESLint config for backend, update .gitignore, add .nvmrc

---
+++ backend/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-types': 'warn',
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+];
